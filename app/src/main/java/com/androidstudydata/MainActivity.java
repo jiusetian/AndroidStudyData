f70b490e92d437e9a81e08330a6e854229bfcc72
seasongao <seasongao@126.com>
2018-10-17 15:55:46 +0800

    有关ContraintLayout的学习

---
+++ app/src/main/java/com/androidstudydata/MainActivity.java
@@ -0,0 +1,22 @@
+package com.androidstudydata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.contraitlayout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ConstraintLayoutActivity.class));
+            }
+        });
+    }
+}
